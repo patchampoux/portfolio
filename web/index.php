2bc0288b18e85519c8f77671fb84e2b7309b6f3c
Patrick Champoux <patrickchampoux@Patricks-MacBook-Pro.local>
2014-06-09 17:00:47 -0400

    Intégration du framework de base et intégration de la page tmp

---
+++ web/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+	<html lang="fr" id="html" class="no-js lt-ie9 lt-ie8">
+<![endif]-->
+<!--[if IE 8]>
+	<html lang="fr" id="html" class="no-js lt-ie9">
+<![endif]-->
+<!--[if gt IE 8]><!-->
+	<html lang="fr" id="html" class="no-js">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+
+    <link rel="stylesheet" href="css/production.min.css">
+
+    <!-- Favicon and Apple Icons -->
+    <link rel="shortcut icon" href="images/icons/favicon.ico">
+    <link rel="apple-touch-icon" href="images/icons/apple-touch-icon.ico">
+    <link rel="apple-touch-icon" size="72x72" href="images/icons/apple-touch-icon-72x72.ico">
+    <link rel="apple-touch-icon" size="114x114" href="images/icons/apple-touch-icon-114x114.ico">
+    
+    <!-- HTML5 Shim, HTML5 printshiv and Respond.js IE8 support of HTML5 elements, printing HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="js/ie/html5shiv-printshiv.js"></script>
+        <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script>
+        document.getElementById("html").className = "";
+    </script>
+</head>
+<body>
+    <header class="l-header">
+        
+    </header>
+
+    <main class="l-main">
+        
+    </main>
+
+    <footer class="l-footer">
+        
+    </footer>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/fallback/jquery-1.11.1.min.js"><\/script>')</script>
+
+    <script src="js/build/production.min.js"></script>
+
+    <!--[if (gte IE 6)&(lte IE 8)]>
+        <script src="js/ie/selectivizr.min.js"></script>
+    <![endif]-->
+</body>
+</html>	
