fafbb535ec28d39d3ccd8b63e63210c489398714
Patrick Champoux <champoux.patrick@gmail.com>
2015-08-09 21:26:50 -0400

    Add line breaks in contact email.

--- web/index.php
+++ web/index.php
@@ -40,7 +40,7 @@ if(isset($_POST['contact-submit'])) {
 
 	if($validated === TRUE)
 	{
-		$body = htmlentities($_POST['contact-name']).'\n'.htmlentities($_POST['contact-email']).'\n\n'.htmlentities($_POST['contact-message']);
+		$body = htmlentities($_POST['contact-name'])."\n".htmlentities($_POST['contact-email'])."\n\n".htmlentities($_POST['contact-message']);
 		$mail = new PHPMailer;
 		$mail->From = htmlentities($_POST['contact-email']);
 		$mail->FromName = htmlentities($_POST['contact-name']);
